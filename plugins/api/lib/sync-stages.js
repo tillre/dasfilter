c8f2c8ce07641a14573c0093188576d03e39fb76
Till Reitemeyer <till@skoni.de>
2014-05-30 17:08:10 +0200

    sync start stage

--- plugins/api/lib/sync-stages.js
+++ plugins/api/lib/sync-stages.js
@@ -4,20 +4,33 @@ var J = require('jski')();
 
 module.exports = function(plugin, cores, resDefs) {
 
-  // create stages for classifications which dont yet exist
+  // create stages which dont yet exist
 
   return function() {
     var classes = [];
 
-    return cores.resources.Classification.view('all', { include_docs: true }).then(function(result) {
-      classes = result.rows.map(function(row) { return row.doc; });
+    // create start page stage when missing
+    cores.resources.StartStage.view('all').then(function(result) {
+      if (result.rows.length > 0) {
+        return null;
+      }
+      var stage = J.createValue(resDefs.StartStage.schema);
+      return cores.resources.StartStage.save(stage).then(function(doc) {
+        plugin.log(['api'], 'stage for start page created');
+      });
 
-      return cores.resources.ClassificationStage.view('all', { include_docs: true });
+    }).then(function() {
+      // create missing classification stage pages
+      return cores.resources.Classification.view('all', { include_docs: true });
+
     }).then(function(result) {
+      classes = result.rows.map(function(row) { return row.doc; });
+      return cores.resources.ClassificationStage.view('all', { include_docs: true });
 
+    }).then(function(result) {
       var stages = {};
       result.rows.forEach(function(row) {
-        stages[row.doc.classification] = row.doc;
+        stages[row.doc.classification.id] = row.doc;
       });
 
       var missingStages = classes.filter(function(cls) {
@@ -29,7 +42,8 @@ module.exports = function(plugin, cores, resDefs) {
       var promises = missingStages.map(function(cls) {
         var stage = J.createValue(resDefs.ClassificationStage.schema);
         stage.title = cls.title;
-        stage.classification = cls._id;
+        stage.classification.id = cls._id;
+        stage.classification.type = cls.type_;
 
         return cores.resources.ClassificationStage.save(stage).then(function(doc) {
           plugin.log(['api'], 'stage for classification created: ' + cls.title);
