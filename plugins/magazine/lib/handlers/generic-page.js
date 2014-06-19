73bf7861e9f9ab5bdb7cab6f0862136be52ebf57
Till Reitemeyer <till@skoni.de>
2014-06-19 15:04:22 +0200

    update pages handler to new models interface

--- plugins/magazine/lib/handlers/generic-page.js
+++ plugins/magazine/lib/handlers/generic-page.js
@@ -4,9 +4,9 @@ module.exports = function(app) {
   return function pageHandler(request, reply) {
     var cls;
 
-    return app.api.getClassifications().then(function(c) {
+    return app.models.classifications.getAll().then(function(c) {
       cls = c;
-      return app.api.getPageBySlug(request.params.page);
+      return app.models.pages.bySlug(request.params.page);
 
     }).then(function(page) {
       app.replyView(request, reply, 'generic-page', {
--- plugins/magazine/lib/models/models.js
+++ plugins/magazine/lib/models/models.js
@@ -2,6 +2,7 @@
 module.exports = function(resources) {
   return {
     articles: require('./articles.js')(resources),
+    pages: require('./pages.js')(resources),
     classifications: require('./classifications.js')(resources),
     stages: require('./stages.js')(resources),
     teasers: require('./teasers.js')(resources)
