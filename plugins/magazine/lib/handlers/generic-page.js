33812f8e9d3dd8398bdeb3690a02ae346be00440
Till Reitemeyer <till@skoni.de>
2014-07-21 01:20:23 +0200

    category header

--- plugins/magazine/lib/handlers/generic-page.js
+++ plugins/magazine/lib/handlers/generic-page.js
@@ -4,8 +4,8 @@ module.exports = function(app) {
   return function pageHandler(request, reply) {
     var cls;
 
-    return app.models.classifications.getAll().then(function(c) {
-      cls = c;
+    return app.models.classifications.getAll().then(function(cs) {
+      cls = cs;
       return app.models.pages.bySlug(request.params.page);
 
     }).then(function(page) {
