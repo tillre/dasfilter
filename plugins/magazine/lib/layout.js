f447561c96c3810e9c35aa5a9388bf0f2367c1da
Till Reitemeyer <till@skoni.de>
2014-07-29 09:01:01 +0200

    teaser styling

--- plugins/magazine/lib/layout.js
+++ plugins/magazine/lib/layout.js
@@ -16,6 +16,19 @@ function getSpanSize(groupSize, i) {
   }
 }
 
+function getPromotedSize(groupSize, i) {
+  switch(groupSize) {
+  case 1: return [6][i];
+  case 2: return [4, 2][i];
+  case 3: return [6, 3, 3][i];
+  case 4: return [6, 2, 2, 2][i];
+  case 5: return [3, 3, 2, 2, 2][i];
+  case 6: return [2, 2, 2, 2, 2, 2][i];
+  default: return 2;
+  }
+}
+
+
 
 function createTeaser(type, span, display, options, groupOptions) {
   return _.merge({
@@ -66,122 +79,128 @@ function createRefs() {
 
 
 function collectGroupRefs(group, refs) {
-    var i, t;
+  var i, t;
+  var getSize = getSpanSize;
 
-    switch(group.type_) {
-    case 'chrono':
-      group.teasers = group.teasers || [];
-      for (i = 0; i < group.numTeasers; ++i) {
-        t = createTeaser('chrono',
-                         getSpanSize(group.numTeasers, i),
-                         'light',
-                         group.teaserOptions);
-        group.teasers.push(t);
+  switch(group.type_) {
+  case 'chrono':
+    group.teasers = group.teasers || [];
+    for (i = 0; i < group.numTeasers; ++i) {
+      t = createTeaser('chrono',
+                       getSize(group.numTeasers, i),
+                       'light',
+                       group.teaserOptions);
+      group.teasers.push(t);
+      refs.addChrono(t);
+    }
+    break;
+
+  case 'pinned':
+    group.teasers.forEach(function(t, i) {
+      // add to chronos when id not set
+      t.span = getSize(group.teasers.length, i);
+      t.display = 'dark';
+      if (t.article.id_) {
+        t.type = 'pinned';
+        t.id = t.article.id_;
+        refs.addPinned(t);
+      } else {
+        t.type = 'chrono';
         refs.addChrono(t);
       }
-      break;
+    });
+    break;
 
-    case 'pinned':
-      group.teasers.forEach(function(t, i) {
-        // add to chronos when id not set
-        t.span = getSpanSize(group.teasers.length, i);
-        t.display = 'dark';
+  case 'tag':
+    group.teasers = [];
+    for (i = 0; i < group.numTeasers; ++i) {
+      t = createTeaser('tag',
+                       getSize(group.numTeasers, i),
+                       'dark',
+                       { id: group.tag.slug },
+                       group.teaserOptions);
+      group.teasers.push(t);
+      refs.addCls('tag', t);
+    }
+    break;
+
+  case 'category':
+    group.teasers = [];
+    for (i = 0; i < group.numTeasers; ++i) {
+      t = createTeaser('category',
+                       getSize(group.numTeasers, i),
+                       'dark',
+                       { id: group.category.id_ },
+                       group.teaserOptions);
+      group.teasers.push(t);
+      refs.addCls('category', t);
+    }
+    break;
+
+  case 'collection':
+    group.teasers = [];
+    for (i = 0; i < group.numTeasers; ++i) {
+      t = createTeaser('collection',
+                       getSize(group.numTeasers, i),
+                       'dark',
+                       { id: group.collection.id_ },
+                       group.teaserOptions);
+      group.teasers.push(t);
+      refs.addCls('collection', t);
+    }
+    break;
+
+  case 'mixed':
+    group.teasers.forEach(function(t, i) {
+      t.span = getSize(group.teasers.length, i);
+      // t.display = 'light mixed';
+      // t.headlineFirst = true;
+
+      switch(t.type_) {
+      case 'chronoTeaser':
+        t.type = 'chrono';
+        t.display = 'mixed light';
+        // t.headlineFirst = false;
+        refs.addChrono(t);
+        break;
+
+      case 'pinnedTeaser':
         if (t.article.id_) {
           t.type = 'pinned';
           t.id = t.article.id_;
+          t.display = 'mixed dark';
           refs.addPinned(t);
         } else {
           t.type = 'chrono';
+          t.display = 'mixed light';
           refs.addChrono(t);
         }
-      });
-      break;
+        break;
 
-    case 'tag':
-      group.teasers = [];
-      for (i = 0; i < group.numTeasers; ++i) {
-        t = createTeaser('tag',
-                         getSpanSize(group.numTeasers, i),
-                         'dark',
-                         { id: group.tag.slug },
-                         group.teaserOptions);
-        group.teasers.push(t);
+      case 'tagTeaser':
+        t.type = 'tag';
+        t.id = t.tag.slug;
+        t.display = 'mixed dark';
         refs.addCls('tag', t);
-      }
-      break;
+        break;
 
-    case 'category':
-      group.teasers = [];
-      for (i = 0; i < group.numTeasers; ++i) {
-        t = createTeaser('category',
-                         getSpanSize(group.numTeasers, i),
-                         'dark',
-                         { id: group.category.id_ },
-                         group.teaserOptions);
-        group.teasers.push(t);
+      case 'categoryTeaser':
+        t.type = 'category';
+        t.id = t.category.id_;
+        t.display = 'mixed dark';
         refs.addCls('category', t);
-      }
-      break;
+        break;
 
-    case 'collection':
-      group.teasers = [];
-      for (i = 0; i < group.numTeasers; ++i) {
-        t = createTeaser('collection',
-                         getSpanSize(group.numTeasers, i),
-                         'dark',
-                         { id: group.collection.id_ },
-                         group.teaserOptions);
-        group.teasers.push(t);
+      case 'collectionTeaser':
+        t.type = 'collection';
+        t.id = t.collection.id_;
+        t.display = 'mixed dark';
         refs.addCls('collection', t);
+        break;
       }
-      break;
-
-    case 'mixed':
-      group.teasers.forEach(function(t, i) {
-        t.span = getSpanSize(group.teasers.length, i);
-        t.display = 'light mixed';
-        t.headlineFirst = true;
-
-        switch(t.type_) {
-        case 'chronoTeaser':
-          t.type = 'chrono';
-          t.display = 'light';
-          t.headlineFirst = false;
-          refs.addChrono(t);
-          break;
-
-        case 'pinnedTeaser':
-          if (t.article.id_) {
-            t.type = 'pinned';
-            t.id = t.article.id_;
-            refs.addPinned(t);
-          } else {
-            t.type = 'chrono';
-            refs.addChrono(t);
-          }
-          break;
-
-        case 'tagTeaser':
-          t.type = 'tag';
-          t.id = t.tag.slug;
-          refs.addCls('tag', t);
-          break;
-
-        case 'categoryTeaser':
-          t.type = 'category';
-          t.id = t.category.id_;
-          refs.addCls('category', t);
-          break;
-
-        case 'collectionTeaser':
-          t.type = 'collection';
-          t.id = t.collection.id_;
-          refs.addCls('collection', t);
-          break;
-        }
-      });
-      break;
-    }
+    });
+    break;
+  }
 }
 
 
