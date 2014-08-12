a8ca09024744dc1fe5036c3b882d7ba08fd0fc80
Till Reitemeyer <till@skoni.de>
2014-08-12 18:04:09 +0200

    return streams from tasks and add nodemon

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,23 @@
 var gulp = require('gulp');
+var nodemon = require('gulp-nodemon');
 var shell = require('gulp-shell');
 
 gulp.task('update-plugin-packages', shell.task([
   'npm update',
 ], { cwd: './plugins/api' }));
+
+
+// pseudo build task for compatibility
+gulp.task('build', function() {});
+
+
+gulp.task('serve', function () {
+  nodemon({
+    script: 'index.js',
+    ext: 'js',
+    watch: ['plugins/api/index.js', 'plugins/api/lib/**'],
+    env: {
+      'NODE_ENV': 'development'
+    }
+  });
+});
