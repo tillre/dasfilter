5a5e5ebed9f418b725355dcce50f0293db85107b
Till Reitemeyer <till@skoni.de>
2014-08-27 14:10:07 +0200

    add s3 url

--- manifest.js
+++ manifest.js
@@ -27,7 +27,7 @@ var manifest = {
       urls: {
         web: env.DF_WEB_URL || 'http://0.0.0.0:8080',
         admin: env.DF_ADMIN_URL || 'http://0.0.0.0:8070',
-        static: env.DF_STATIC_URL || 'http://0.0.0.0:8060',
+        static: env.DF_STATIC_URL || 'http://static.dev.dasfilter.com.s3-website-eu-west-1.amazonaws.com',
         api: env.DF_API_URL || 'http://0.0.0.0:8050'
       },
 
@@ -40,33 +40,5 @@ var manifest = {
     }
   }
 };
-
-// plugins
-
-// manifest.plugins['hapi-auth-cookie'] = {};
-
-// manifest.plugins['good'] = {
-//   subscribers: {
-//     console: ['ops', 'request', 'log', 'error']
-//   }
-// };
-
-// manifest.plugins[Path.resolve('plugins/admin')] = {
-//   debug: env.NODE_ENV !== 'production',
-
-//   urls: {
-//     web: env.DF_WEB_URL || 'http://0.0.0.0:8080',
-//     admin: env.DF_ADMIN_URL || 'http://0.0.0.0:8070',
-//     static: env.DF_STATIC_URL || 'http://0.0.0.0:8060',
-//     api: env.DF_API_URL || 'http://0.0.0.0:8050'
-//   },
-
-//   staticDir: Path.join('static'),
-//   imagesDir: Path.join('static/images'),
-//   assetsDir: Path.join('static/assets'),
-
-//   cookiePassword: env.DF_COOKIE_PASS || 'cookie-password',
-//   apiKey: env.DF_API_KEY || 'api-key'
-// };
 
 module.exports = manifest;
